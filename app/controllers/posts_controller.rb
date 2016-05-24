8d822af6bebef747bc1232a508651158555dc0d7
Brad Lindgren <email": null>
2016-05-23 20:03:19 -0500

    Routes Written Out

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,37 +1,41 @@
 class PostsController < ApplicationController
-  before_action :find_post, except: [:create]
+  # before_action :find_post, except: [:create]
   # before_action :is_owner?, except: [:create, :show]
   # before_action :is_user?, except: [:show]
 
   def create
     @post = Post.new(post_params)
     if @post.save
-      redirect_to category_post_path(@post.category, @post.id)
+      redirect_to "/categories/#{@post.category_id}/posts/#{@post.id}"
     else
       @errors = @errors.post.full_messages
-      render category_post_path(@post.category, @post.id)
+      render "/categories/#{@post.category_id}/posts/#{@post.id}"
     end
   end
 
   def show
+    @post = Post.find(params[:post_id])
   end
 
   def edit
+    @post = Post.find(params[:post_id])
     @category = @post.category
   end
 
   def update
+    @post = Post.find(params[:post_id])
     if @post.update_attributes(post_params)
-      redirect_to category_post_path
+      redirect_to "/categories/#{@post.category_id}/posts/#{@post.id}"
     else
       errors = @errors.post.full_messages
-      render edit_category_post_path
+      render "/categories/#{@post.category_id}/posts/#{@post.id}/edit"
     end
   end
 
   def destroy
+    @post = Post.find(params[:post_id])
     @post.destroy
-    redirect_to categories_path
+    redirect_to "/categories/index"
   end
 
   private
